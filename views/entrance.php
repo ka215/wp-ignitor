1e1ca030b677cb59b5b6714f07684dc54437bbd5
ka2 <ka2@ka2.org>
2020-12-18 10:10:19 +0900

    resolved #2

--- views/entrance.php
+++ views/entrance.php
@@ -10,10 +10,10 @@ define( 'WPIGNITOR_LOGIN_PAGE_DIR', str_replace( DIRECTORY_SEPARATOR, '/', dirna
 define( 'WPIGNITOR_LOGIN_PAGE_FILE', basename( $_SERVER['SCRIPT_FILENAME'] ) );
 define( 'WPIGNITOR_LOGIN_PAGE_URL', str_replace( $_docroot, '', WPIGNITOR_LOGIN_PAGE_DIR ) .'/'. WPIGNITOR_LOGIN_PAGE_FILE );
 define( 'WPIGNITOR_LOGIN_CREDENTIAL', hash( 'sha512', 'wp-ignitor@'. $_SERVER['HTTP_HOST'] .':'. WPIGNITOR_LOGIN_PAGE_DIR .'/'. WPIGNITOR_LOGIN_PAGE_FILE ) );
-$matches = glob( $_docroot .'/*/wp-lo{ad,gin}.php', GLOB_BRACE );
+$matches = glob( $_docroot .'/*/wp-login.php', GLOB_BRACE );
 $core_files = [];
 foreach ( $matches as $file_path ) {
-    if ( in_array( basename( $file_path ), [ 'wp-load.php', 'wp-login.php' ], true ) ) {
+    if ( in_array( basename( $file_path ), [ 'wp-login.php' ], true ) ) {
         $core_files[basename( $file_path )] = str_replace( DIRECTORY_SEPARATOR, '/', $file_path );
     }
 }
@@ -21,11 +21,6 @@ if ( ! empty( $core_files ) && isset( $core_files['wp-login.php'] ) ) {
     $origin_login_file = str_replace( DIRECTORY_SEPARATOR, '/', $matches[0] );
     require_once $core_files['wp-login.php'];
 } else {
-    require_once $core_files['wp-load.php'];
-    global $wp_query;
-    $wp_query->set_404();
-    status_header( '404' );
-    nocache_headers();
-    include( get_query_template( '404' ) );
+    header( 'HTTP/1.0 404 Not Found' );
     exit;
 }
